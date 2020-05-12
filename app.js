4bd6be57462beec3d4c961e26d0ec64db76cd404
ahmedfawzy93 <44768497+ahmedfawzy93@users.noreply.github.com>
2020-05-12 17:26:13 +0200

    Update app.js
    "removing the database URLs and keeping the environment variable"

--- app.js
+++ app.js
@@ -29,15 +29,6 @@ mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useUnifiedTop
 	console.log("ERROR:", err.message)	
 });
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-
-// mongoose.connect("mongodb+srv://afawzy:yelpyc2mpyd2t2@cluster0-chvov.mongodb.net/yelp_camp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(function(){
-// 	console.log("Connected to DB");
-// }). catch (err => {
-// 	console.log("ERROR:", err.message)	
-// });
-
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
